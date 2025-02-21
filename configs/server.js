487413b40213b42e25e890c0fa9566a81c04e840
OmarMirandaXD <omar.xocoy2007@gmail.com>
2025-02-20 20:36:51 -0600

    Update server.js

--- configs/server.js
+++ configs/server.js
@@ -6,13 +6,11 @@ import helmet from "helmet"
 import morgan from "morgan"
 import { dbConnection } from "./mongo.js"
 import authRoutes from "../src/auth/auth.routes.js";
-import clienteRoutes from "../src/clientes/cliente.routes.js";
-import apiLimiter from "../src/helpers/rate-limit-validator.js"
-
-
+import usuarioRoutes from "../src/usuarios/usuario.routes.js";
+import apiLimiter from "../src/middlewares/rate-limit-validator.js"
 
 const middlewares = (app) => {
-    app.use(express.urlencoded({extended: false}))
+    app.use(express.urlencoded({ extended: false }))
     app.use(express.json())
     app.use(cors())
     app.use(helmet())
@@ -20,29 +18,29 @@ const middlewares = (app) => {
     app.use(apiLimiter)
 }
 
-const conectarDB = async () =>{
-    try{
+const conectarDB = async () => {
+    try {
         await dbConnection()
-    }catch(err){
+    } catch (err) {
         console.log(`Database connection failed: ${err}`)
         process.exit(1)
     }
 }
 
 const routes = (app) => {
-    app.use("/uniNet/v1/auth", authRoutes);
-    app.use("/uniNet/v1/cliente", clienteRoutes);
+    app.use("/practicasS/v1/auth", authRoutes);
+    app.use("/practicasS/v1/usuario", usuarioRoutes);
 }
 
 export const initServer = () => {
     const app = express()
-    try{
+    try {
         middlewares(app)
         conectarDB()
         routes(app)
         app.listen(process.env.PORT)
         console.log(`Server running on port ${process.env.PORT}`)
-    }catch(err){
+    } catch (err) {
         console.log(`Server init failed: ${err}`)
     }
 }
