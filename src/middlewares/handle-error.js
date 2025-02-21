798fabdd06f51a4432b508c39a9a81cc9da25e78
Omar <omar.xocoy2007@gmail.com>
2025-02-20 21:11:53 -0600

    update handle

--- src/middlewares/handle-error.js
+++ src/middlewares/handle-error.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const handleErrors = (err, req, res, next) => {
     if (err.status === 400 || err.errors) {
         return res.status(400).json({
@@ -11,17 +10,4 @@ export const handleErrors = (err, req, res, next) => {
         message: err.message
     });
 }
-=======
-export const handleErrors = (err, req, res, next) => {
-    if (err.status === 400 || err.errors) {
-        return res.status(400).json({
-            success: false,
-            errors: err.errors
-        });
-    }
-    return res.status(500).json({
-        success: false,
-        message: err.message
-    });
-}
->>>>>>> usuarios
+
